a1ee215d6beff695475f41fd060dcc61755c5be1
Ivett Ördög <devillmeister@gmail.com>
2019-09-01 21:18:01 +0200

    Update Player.java

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 0;
+        return 70000;
     }
 
     public static void showdown(JsonElement game) {
