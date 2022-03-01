7f9ec54a74676fb34ee2bc52e1cef97b084bddff
LarsEckart <lars.eckart@hey.com>
2022-03-01 21:54:21 +0200

    shameless green

--- src/main/java/org/leanpoker/player/ChenCalculator.java
+++ src/main/java/org/leanpoker/player/ChenCalculator.java
@@ -5,6 +5,15 @@ import java.util.List;
 public class ChenCalculator {
 
     public static int calculate(List<Card> cards) {
-        return 6;
+        int result = 0;
+        Card higherCard = cards.get(1);
+        int rank = Integer.parseInt(higherCard.getRank());
+        double halfRank = rank / 2.0;
+        if (higherCard.getSuit().equals(cards.get(0).getSuit())) {
+            result += 2;
+        }
+        int i = (int) Math.ceil(halfRank);
+        result = result + i;
+        return result;
     }
 }
